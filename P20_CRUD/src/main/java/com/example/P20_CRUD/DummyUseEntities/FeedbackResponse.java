f069c4d057f1fb9e8c7a93753632fe76dcd1a06c
Sanket70200 <sanketpatil9850@gmail.com>
2026-07-28 23:16:11 +0530

    Add feedback response DTO

---
+++ P20_CRUD/src/main/java/com/example/P20_CRUD/DummyUseEntities/FeedbackResponse.java
@@ -0,0 +1,17 @@
+package com.example.P20_CRUD.DummyUseEntities;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class FeedbackResponse {
+    private Integer feedbackId;
+    private int packageId;
+    private Integer rating;
+    private String feedbackDesc;
+    private String reviewerName;
+    private LocalDateTime createdAt;
+}
